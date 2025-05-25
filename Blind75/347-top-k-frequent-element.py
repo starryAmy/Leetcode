41a15423fa8d2b6af3b9e89d613759b14f26663a
amy huang <flower850603@gmail.com>
2025-05-25 15:47:08 +0900

    added footnote for 347 solution

--- Blind75/347-top-k-frequent-element.py
+++ Blind75/347-top-k-frequent-element.py
@@ -9,9 +9,11 @@ class Solution(object):
         freq = [[] for i in range(len(nums) + 1)] # sort according to freq
         res = []
         for num in nums:
-            count[num] = count.get(num, 0) + 1
+            count[num] = count.get(num, 0) + 1 # if num is not in count, count[num] = 0
+        # get every pair of count and sort them according to freq
         for n, c in count.items():
             freq[c].append(n)
+        # traverse freq from high to low
         for i in range(len(freq) - 1, 0, -1):
             for num in freq[i]:
                 res.append(num)
