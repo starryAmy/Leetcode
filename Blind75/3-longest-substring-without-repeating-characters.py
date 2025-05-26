47cd7427d57f437969162af81534c4fc60d0b486
amy huang <flower850603@gmail.com>
2025-05-26 23:24:25 +0900

    added sidenotes for solution 3

--- Blind75/3-longest-substring-without-repeating-characters.py
+++ Blind75/3-longest-substring-without-repeating-characters.py
@@ -4,11 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # sliding window
         char = set()
         l, r = 0, 0
         max_length = 0
         while r < len(s):
-            while s[r] in char:
+            while s[r] in char: # keep looping until there is no duplicate
                 char.remove(s[l])
                 l += 1
             max_length = max(max_length, r - l + 1)
