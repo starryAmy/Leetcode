74634c0c0189cfbf265c3f4c825d3824da9ff933
amy huang <flower850603@gmail.com>
2025-05-26 23:23:42 +0900

    added solution for 3

---
+++ Blind75/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char = set()
+        l, r = 0, 0
+        max_length = 0
+        while r < len(s):
+            while s[r] in char:
+                char.remove(s[l])
+                l += 1
+            max_length = max(max_length, r - l + 1)
+            char.add(s[r])
+            r += 1
+        return max_length
