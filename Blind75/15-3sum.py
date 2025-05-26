c4d2cf048f120b5af18b7d184dd6df77dfcdc050
amy huang <flower850603@gmail.com>
2025-05-26 22:21:56 +0900

    amended solution for 15

--- Blind75/15-3sum.py
+++ Blind75/15-3sum.py
@@ -15,15 +15,16 @@ class Solution(object):
                 continue
             l = i + 1
             r = len(nums) - 1
-            threeSum = nums[i] + nums[l] + nums[r]
-            if threeSum > 0:
-                r -= 1
-            elif threeSum < 0:
-                l += 1
-            else:
-                res.append([nums[i], nums[l], nums[r]])
-                r -= 1
-                l += 1
-                while nums[l - 1] == nums[l] and l < r: # check if second element is duplicate
+            while l < r:
+                threeSum = nums[i] + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
                     l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    r -= 1
+                    l += 1
+                    while nums[l - 1] == nums[l] and l < r: # check if second element is duplicate
+                        l += 1
         return res
