3d99fb33b9dafffe0a34af390dee1798ae196e0f
amy huang <flower850603@gmail.com>
2025-05-26 23:04:12 +0900

    amended solution for 121

--- Blind75/121-best-time-to-buy-and-sell.py
+++ Blind75/121-best-time-to-buy-and-sell.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        # sliding window
         l, r = 0, 1
         res = 0
         while r < len(prices):
             if prices[l] <= prices[r]:
                 res = max(res, prices[r] - prices[l])
-                r += 1
             else:
-                l += 1
+                l = r # we found prices[r] is lower, so use prices[r] instead
+            r += 1 # no matter what, right pointer always needs to move one step further
         return res
